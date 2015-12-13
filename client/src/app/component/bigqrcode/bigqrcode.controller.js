1f05fdb329378323eadc2907f0619f9c323a5e89
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-13 12:39:58 +0100

    AwrapTemplatess scaling in big QR code

--- client/src/app/component/bigqrcode/bigqrcode.controller.js
+++ client/src/app/component/bigqrcode/bigqrcode.controller.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('nw-admin').controller('BigQRCodeCtrl', function ($http) {
+  angular.module('nw-admin').controller('BigQRCodeCtrl', function ($http, $timeout) {
     'ngInject';
 
     var ctrl = this;
@@ -12,29 +12,43 @@
         .get('/api/category')
         .then(function(response){
           ctrl.categories = response.data;
+          if(ctrl.categories && ctrl.categories.length>0){
+            ctrl.selectCategory(ctrl.categories[0].id)
+          }
         });
     }
 
+    ctrl.selectCategory = function(id){
+      $http.get('/api/category/' + id)
+        .then(function (response) {
+          ctrl.qrs = response.data.qrcodes
+            .filter(function(elt){
+              return elt.big!==true;
+            });
+
+          var mainQr = response.data.qrcodes
+            .filter(function(elt){
+              return elt.big===true;
+            })[0];
+
+          $timeout(function(){
+            var svg = document.querySelector('.bigqrcode');
+            var dimension = svg.clientHeight>svg.clientWidth ? svg.clientWidth : svg.clientHeight;
+
+            var ratio = dimension/(mainQr.dimension+4);
+
+            ctrl.qrs.forEach(function(elt){
+              elt.x = elt.x*ratio+6;
+              elt.y = elt.y*ratio+6;
+              elt.scaleDimension = ratio;
+              elt.scaleRatio = ratio/elt.dimension;
+            });
+          });
+
+        });
+    };
     //ctrl.refresh = function(){
-    //  $http.get('/api/category/' + ctrl.id)
-    //    .then(function (response) {
-    //      ctrl.entity = response.data;
-    //
-    //      ctrl.svg = response.data.qrcodes.filter(function(elt){
-    //        return elt.big===true;
-    //      })[0];
-    //
-    //      ctrl.qrs = response.data.qrcodes
-    //        .filter(function(elt){
-    //          return elt.big!==true;
-    //        });
-    //
-    //      ctrl.qrs.forEach(function(elt){
-    //          elt.x = elt.x*29+6;
-    //          elt.y = elt.y*29+6;
-    //        });
-    //    });
-    //};
+
 
     refresh();
   });
