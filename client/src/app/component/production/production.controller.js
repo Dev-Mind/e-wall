1a8d416cd6ce32560d6265d1fb0491c68d4676f7
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-27 09:26:56 +0100

    First tab used to select a category

--- client/src/app/component/production/production.controller.js
+++ client/src/app/component/production/production.controller.js
@@ -2,12 +2,22 @@
 
   'use strict';
 
-  angular.module('ew-admin').controller('ProductionCtrl', function () {
+  angular.module('ew-admin').controller('ProductionCtrl', function ($http) {
     'ngInject';
 
     var ctrl = this;
 
+    $http
+      .get('/api/public/category')
+      .then(function(response){
+        ctrl.categories = response.data;
+      });
 
+
+    ctrl.selectCategory = function(category){
+      console.log(category)
+      ctrl.currentTab = 'content';
+    }
   });
 
 })();
