8b21d21683b0c807b446d17374ab9ae720673d05
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-30 19:42:57 +0100

    Init method must be transactionnal

--- server/src/main/java/fr/emse/ewall/EWallInitializer.java
+++ server/src/main/java/fr/emse/ewall/EWallInitializer.java
@@ -12,6 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Initialize data if db is empty
@@ -31,6 +32,7 @@ public class EWallInitializer {
     private AuthorityRepository authorityRepository;
 
     @PostConstruct
+    @Transactional
     public void init() {
         if(categoryRepository.count()==0){
             logger.info("This is the first start of the application. We create the different categories");
